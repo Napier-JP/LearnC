247bc3fca4b9f5c6c52ce36d7dc01f599a36abd0
Napier <26354173+Napier-JP@users.noreply.github.com>
2019-08-25 12:35:16 +0900

    Update threads_count.cpp

--- C++/threads_count.cpp
+++ C++/threads_count.cpp
@@ -2,33 +2,39 @@
 #include <omp.h>
 
 int main() {
-    #pragma omp parallel for
+    std::vector<char> alphabets(26);
+    char c = 'A';
+    for (int i = 0; i < 26; ++i) {
+        alphabets[i] = c;
+        ++c;
+    }
+#pragma omp parallel for
     for (int i = 0; i < 20; ++i) {
-        printf("%d / %d\n", omp_get_thread_num(), omp_get_max_threads());
+        printf("%c %d / %d\n", alphabets[i], omp_get_thread_num(), omp_get_max_threads());
     }
 
     return 0;
 }
 
 /* Output on WSL Ubuntu w/ i7-6700 (4C8T) was:
-0 / 8
-0 / 8
-0 / 8
-4 / 8
-4 / 8
-3 / 8
-3 / 8
-3 / 8
-2 / 8
-2 / 8
-2 / 8
-5 / 8
-5 / 8
-7 / 8
-7 / 8
-6 / 8
-6 / 8
-1 / 8
-1 / 8
-1 / 8
+M 4 / 8
+N 4 / 8
+S 7 / 8
+T 7 / 8
+D 1 / 8
+E 1 / 8
+F 1 / 8
+G 2 / 8
+H 2 / 8
+I 2 / 8
+A 0 / 8
+B 0 / 8
+C 0 / 8
+J 3 / 8
+K 3 / 8
+L 3 / 8
+Q 6 / 8
+R 6 / 8
+O 5 / 8
+P 5 / 8
 */
